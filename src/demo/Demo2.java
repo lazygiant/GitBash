9d77d1c238f688576aece04e447a178db9306897
Abhay3011994 <abhay3011994@gmail.com>
2024-08-21 10:31:12 +0530

    Commit3

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -4,7 +4,9 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 	
-System.out.println("hello");
+     System.out.println("hello");
+     System.out.println("hello world");
+     
 	}
 
 }
